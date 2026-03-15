17f5b18e5aa138f7ab05024a637c9b43dd551506
erick <erick@LEGI0SORIO>
2026-03-14 18:50:21 -0600

    Primer commit del proyecto

--- 1 proyecto-PC/Program.cs
+++ 1 proyecto-PC/Program.cs
@@ -1,1 +1,41 @@
-﻿
+﻿int opcion = 0;
+do
+{
+    Console.WriteLine("MENU");
+    Console.WriteLine("1. Evaluar nuevo contenido");
+    Console.WriteLine("2. Mostrar reglas del sistema");
+    Console.WriteLine("3. Mostrar estadísticas de la sesión");
+    Console.WriteLine("4. Reiniciar estadísticas");
+    Console.WriteLine("5. Salir");
+    Console.WriteLine("Elegir la opcion a realizar: ");
+    opcion=int.Parse(Console.ReadLine());
+    switch (opcion)
+    {
+        case 1:
+            {
+                break;
+            }
+        case 2:
+            {
+                break;
+            }
+        case 3:
+            {
+                break;
+            }
+        case 4:
+            {
+                break;
+            }
+        case 5:
+            {
+                Console.WriteLine("Salida");
+                break;
+            }
+        default:
+            {
+                Console.WriteLine("Opcion Invalida");
+                break;
+            }
+    }
+} while (opcion != 5);
